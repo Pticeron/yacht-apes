0b9908166745c923c4fcb3a7637ce402c27ac3dd
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2024-03-20 18:38:29 +0200

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Container } from "components/Container/Container"; 
+import { Navigation } from "components/Navigation/Navigation";
+
+export const Header = () => {
+    return (
+      <>
+        <Container>
+          <Navigation />
+        </Container>
+      </>
+    );
+  };
+  
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,71 @@
+// import { useState, useEffect } from 'react';
+// import { NavLink } from 'react-router-dom';
+
+
+export const Navigation = () => {
+//   const [activeLink, setActiveLink] = useState('home');
+//   const [scrolled, setScrolled] = useState(false);
+
+//   useEffect(() => {
+//     const onScroll = () => {
+//       if (window.scrollY > 50) {
+//         setScrolled(true);
+//       } else {
+//         setScrolled(false);
+//       }
+//     };
+
+//     window.addEventListener('scroll', onScroll);
+
+//     return () => window.removeEventListener('scroll', onScroll);
+//   }, []);
+
+//   const onUpdateActiveLink = value => {
+//     setActiveLink(value);
+//   };
+
+//   return (
+//     <div>
+//       <nav >
+//         <div>
+//           <NavLink to="/" 
+//           className={
+//             activeLink === 'logo' ? 'active navbar-link' : 'navbar-link'
+//           }
+//           onClick={() => onUpdateActiveLink('home')}>Logo</NavLink>
+//           <ul>
+//             <li>
+//               <NavLink to="/">ABOUT</NavLink>
+//             </li>
+//             <li>
+//               <NavLink to="/">M-map</NavLink>
+//             </li>
+//             <li>
+//               <NavLink to="/">Faq</NavLink>
+//             </li>
+//             <li>
+//               <NavLink to="/">ARTS</NavLink>
+//             </li>
+//             <li>
+//               <NavLink to="/">MINT</NavLink>
+//             </li>
+//           </ul>
+//           <button>Close</button>
+//         </div>
+//         <div>
+//           <ul>
+//             <li>
+//               <svg></svg>
+//             </li>
+//             <li>
+//               <svg></svg>
+//             </li>
+//             <li>
+//               <svg></svg>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+};
