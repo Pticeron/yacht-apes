32bf5ec0985bcb2761a0cfd65c5e1124897d9085
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2024-03-23 11:04:03 +0200

    add data

---
+++ src/components/data/faq.jsx
@@ -0,0 +1,37 @@
+import Apes1 from "../assets/img/faq/img-1.png";
+import Apes2 from "../assets/img/faq/img-2.png";
+import Apes3 from "../assets/img/faq/img3.png";
+import Apes4 from "../assets/img/faq/img-4.png";
+
+
+
+export const faq = [
+    {
+      queue: '[ 1 ]',
+      question: 'WHAT IS AN NFT COLLECTION?',
+      answer:
+        'An NFT collection is a group of unique digital assets, each represented by a non-fungible token, typically created around a specific theme or style.',
+      image: Apes1,
+    },
+    {
+      queue: '[ 2 ]',
+      question: 'HOW DO I PURCHASE NFTS FROM A COLLECTION?',
+      answer:
+        'To purchase NFTs from a collection, you typically need to use cryptocurrency on a blockchain-based marketplace.',
+      image: Apes2,
+    },
+    {
+      queue: '[ 3 ]',
+      question: 'CAN I SELL OR TRADE NFTS FROM A COLLECTION?',
+      answer:
+        'Yes, you can sell or trade NFTs from a collection like you would other digital assets.',
+      image: Apes3,
+    },
+    {
+      queue: '[ 4 ]',
+      question: 'WHAT RIGHTS DO I HAVE AS AN OWNER OF AN NFT?',
+      answer:
+        "As an NFT owner, you can own, transfer, potentially access exclusive content, resell, but don't automatically get copyright or intellectual property rights.",
+      image: Apes4,
+    },
+  ];
