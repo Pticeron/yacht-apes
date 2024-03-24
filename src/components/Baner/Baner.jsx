f3d565da0345a7c33b2897b5c19fdb8656a8bdf6
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2024-03-24 20:12:55 +0200

    fixed about

--- src/components/Baner/Baner.jsx
+++ src/components/Baner/Baner.jsx
@@ -1,5 +1,34 @@
-function Baner() {
+import banners from '../../data/banners.json';
+import { AddSharp } from '../assets/img/icons/Icons';
+import {
+    BannerContainer,
+  MovingLine,
+  MovingLineContainer
+} from './Baner.styled';
 
+function Baner() {
+    const renderBanners = () => {
+        return banners.map((banner, index) => (
+          <li key={index}>
+            <p>{banner.text}</p>
+            <AddSharp />
+          </li>
+        ));
+      };
+    
+      const renderMovingLineContainers = (count) => {
+        const movingLineContainers = [];
+        for (let i = 0; i < count; i++) {
+          movingLineContainers.push(
+            <MovingLineContainer key={i}>
+              <MovingLine aria-hidden={i > 0}>{renderBanners()}</MovingLine>
+            </MovingLineContainer>
+          );
+        }
+        return movingLineContainers;
+      };
+    
+      return <BannerContainer>{renderMovingLineContainers(3)}</BannerContainer>;
 }
 
 export default Baner;
---
+++ src/components/Baner/Baner.styled.jsx
@@ -0,0 +1,52 @@
+import styled, { keyframes } from 'styled-components';
+
+const moveLineAnimation = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(calc(-100% - 24px));
+  }
+`;
+
+export const BannerContainer = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  background-color: var(--accent-color);
+  overflow: hidden;
+  display: flex;
+  user-select: none;
+  gap: 24px;
+`;
+
+export const MovingLineContainer = styled.div``;
+
+export const MovingLine = styled.ul`
+  display: flex;
+  gap: 24px;
+  height: 52px;
+  overflow: hidden;
+  animation: ${moveLineAnimation} 15s linear infinite;
+  li {
+    display: flex;
+    gap: 24px;
+    align-items: center;
+    color: var(--main-font-color);
+    font-size: 18px;
+    font-weight: bold;
+    text-wrap: nowrap;
+    white-space: nowrap;
+    color: var(--main-font-color);
+    font-family: 'RightGrotesk-CompactBlack';
+    font-size: 36px;
+    font-weight: 900;
+    line-height: 1;
+    text-align: left;
+    text-transform: uppercase;
+    svg {
+      width: 36px;
+      height: 36px;
+    }
+  }
+`;
